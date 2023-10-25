98e91f3effb233b2598bfd9cc775a4052d829eac
kosinovna-1 <kosinov.n@gmail.com>
2023-10-25 19:33:05 +0300

    feat(main): add sr-1 to lab09

---
+++ labs/lab09/sr-1.asm
@@ -0,0 +1,49 @@
+%include 'in_out.asm'
+
+SECTION .data
+	msg0 db "Вариант I",0
+	msg1 db "Функция: f(x) = 2x+15 ",0
+	msg2 db "Результат: ",0
+
+SECTION .text
+	global _start
+
+_start:
+	; ------- Выод приглашающих сообщений
+	mov eax, msg0
+	call sprintLF
+	mov eax, msg1
+	call sprintLF
+	
+	; ------- Подготовка стека аргументов и буфера для хранения промежуточных сумм
+	pop ecx 	
+	pop edx 	
+	sub ecx,1 	
+	mov esi, 0 	
+	
+	; ------- Проверяем, есть ли аргументы
+	cmp ecx,0h
+	jz _end
+
+	; ------- Считаем в цикле f(x) и прибавляем к буферу esi
+next:
+	pop eax 	; извлекаем следующий аргумент из стека
+	call atoi 	; преобразуем символ в число
+	call _func	; считаем выражение f(x)
+	add esi, eax
+	loop next 	; переход к обработке следующего аргумента
+	
+	; ------- Вывод результата работы программы
+_end:
+	mov eax, msg2 	; вывод сообщения "Результат: "
+	call sprint
+	mov eax, esi 	; записываем произведение в регистр `eax`
+	call iprintLF 	; печать результата
+	call quit 	; завершение программы
+	
+	; ------- Считтаем значение f(x)
+_func:
+	mov ebx, 2
+	mul ebx
+	add eax, 15
+	ret
